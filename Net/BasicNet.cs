a2b568d15d5a95fead8f0663a985dd01ebf2b5a0
常青园晚 <50760269+Dynesshely@users.noreply.github.com>
2022-01-27 23:09:00 +0800

    uploaded all source files at once

---
+++ Net/BasicNet.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.IO;
+
+namespace BasicHelper.Net
+{
+    public class BasicNet
+    {
+        /// <summary>
+        /// 检验是否拥有网络连接
+        /// </summary>
+        /// <param name="target">测试的目标</param>
+        /// <param name="waitTime">等待时间</param>
+        /// <returns>是否拥有网络连接</returns>
+        public static bool IsWebConected(string target, int waitTime)
+        {
+            try
+            {
+                Ping objPingSender = new Ping();
+                PingOptions objPinOptions = new PingOptions
+                {
+                    DontFragment = true
+                };
+                string data = "";
+                byte[] buffer = Encoding.UTF8.GetBytes(data);
+                PingReply objPinReply = objPingSender.Send(target, waitTime, buffer, objPinOptions);
+                string strInfo = objPinReply.Status.ToString();
+                if (strInfo == "Success")
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 返回网络连接失败的原因
+        /// </summary>
+        /// <param name="target">测试目标</param>
+        /// <param name="waitTime">等待时间</param>
+        /// <returns>返回失败原因</returns>
+        public static Exception WebConectionError(string target, int waitTime)
+        {
+            try
+            {
+                Ping objPingSender = new Ping();
+                PingOptions objPinOptions = new PingOptions
+                {
+                    DontFragment = true
+                };
+                string data = "";
+                byte[] buffer = Encoding.UTF8.GetBytes(data);
+                PingReply objPinReply = objPingSender.Send(target, waitTime, buffer, objPinOptions);
+                string strInfo = objPinReply.Status.ToString();
+                return null;
+            }
+            catch (Exception result)
+            {
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// 从服务器下载文件
+        /// </summary>
+        /// <param name="serverFilePath">服务器上的文件位置</param>
+        /// <param name="targetPath">存储到本地的文件位置</param>
+        public static void DownloadFile(string serverFilePath, string targetPath)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(serverFilePath);
+            WebResponse respone = request.GetResponse();
+            Stream netStream = respone.GetResponseStream();
+            using (Stream fileStream = new FileStream(targetPath, FileMode.Create))
+            {
+                byte[] read = new byte[1024];
+                int realReadLen = netStream.Read(read, 0, read.Length);
+                while (realReadLen > 0)
+                {
+                    fileStream.Write(read, 0, realReadLen);
+                    realReadLen = netStream.Read(read, 0, read.Length);
+                }
+                netStream.Close();
+                fileStream.Close();
+            }
+        }
+
+        /// <summary>
+        /// 从服务器下载文件
+        /// </summary>
+        /// <param name="serverFilePath">服务器上的文件位置</param>
+        /// <param name="targetPath">存储到本地的文件位置</param>
+        public static void WebDownloadFile(string serverFilePath, string targetPath)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.DownloadFile(serverFilePath, targetPath);
+            }
+        }
+    }
+}
