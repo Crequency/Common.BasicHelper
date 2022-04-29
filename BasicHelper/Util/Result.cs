d13d3a76e7db4ffeabd0defa6264dfe43188b8b2
Dynesshely <catrol@qq.com>
2022-04-29 08:39:41 +0800

    📦 Struct: Clear

---
+++ BasicHelper/Util/Result.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicHelper.Util
+{
+    internal class Result<T> : Exception
+    {
+
+        private bool hasProblem = false;
+
+        /// <summary>
+        /// 是否存在问题
+        /// </summary>
+        public bool HasProblem
+        {
+            get { return hasProblem; }
+            set { hasProblem = value; }
+        }
+
+        private object returnResult = string.Empty;
+
+        /// <summary>
+        /// 返回结果
+        /// </summary>
+        public object ReturnResult
+        {
+            get { return returnResult; }
+            set { returnResult = value; }
+        }
+
+        private Type returnType = typeof(string);
+
+        /// <summary>
+        /// 返回值类型
+        /// </summary>
+        public Type ReturnType
+        {
+            get { return returnType; }
+            set { returnType = value; }
+        }
+
+        /// <summary>
+        /// 异常状态下的构造函数
+        /// </summary>
+        /// <param name="message">异常消息</param>
+        /// <param name="hasProblem">是否存在问题, 默认存在</param>
+        public Result(string message, bool hasProblem = true) : base(message) => HasProblem = hasProblem;
+
+        /// <summary>
+        /// 正常状态下的构造函数
+        /// </summary>
+        /// <param name="result">返回结果</param>
+        public Result(T result)
+        {
+            if (result != null)
+            {
+                ReturnResult = result;
+                ReturnType = typeof(T);
+            }
+        }
+    }
+}
