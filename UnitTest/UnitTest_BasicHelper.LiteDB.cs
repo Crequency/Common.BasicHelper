a3533c9866aeea2f753257b05b1f272bb34d0d83
Dynesshely <catrol@qq.com>
2022-05-14 15:52:18 +0800

    📦 Struct(UnitTest): for LiteDB

---
+++ UnitTest/UnitTest_BasicHelper.LiteDB.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BasicHelper.Util;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class BasicHelper_LiteDB
+    {
+        [TestMethod]
+        public void BasicFunctionTest()
+        {
+
+        }
+    }
+}
