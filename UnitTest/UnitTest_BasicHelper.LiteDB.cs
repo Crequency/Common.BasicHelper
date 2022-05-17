85f4ff6cd85beca68494302b249e4d9cc582874f
Dynesshely <catrol@qq.com>
2022-05-18 00:18:46 +0800

    ✔️ Test: Update and Delete

--- UnitTest/UnitTest_BasicHelper.LiteDB.cs
+++ UnitTest/UnitTest_BasicHelper.LiteDB.cs
@@ -26,15 +26,15 @@ namespace UnitTest
             {
                 (int)test1.Add(new object[3]
                 {
-                    1, "白学", "日冕大班"
+                    1, "白学  ", "日冕大班"
                 }).ReturnResult,
                 (int)test1.Add(new object[3]
                 {
-                    2, "岚依", "日冕大班"
+                    2, "岚依  ", "日冕大班"
                 }).ReturnResult,
                 (int)test1.Add(new object[3]
                 {
-                    3, "夏尔", "日冕向日葵班"
+                    3, "夏尔  ", "日冕向日葵班"
                 }).ReturnResult,
                 (int)test1.Add(new object[3]
                 {
@@ -45,21 +45,26 @@ namespace UnitTest
                     5, "常青园晚", "日冕小小班"
                 }).ReturnResult
             };
+            test1.Update(4, "Name", "1V/1A");
+            test1.Delete(5);
             foreach (string keys in test1.KeysProperty.Keys)
                 Console.Write($"{keys}\t");
             Console.WriteLine();
             foreach (int id in ids)
             {
-                var result = ((manager
-                    .GetDataBase("Default").ReturnResult as DataBase)
-                    .GetTable("test1").ReturnResult as DataTable)
-                    .Query(id).ReturnResult as List<object>;
-                foreach (var item in result)
+                if (test1.Exist(id))
                 {
-                    Console.Write($"{item}\t");
+                    var result = ((manager
+                        .GetDataBase("Default").ReturnResult as DataBase)
+                        .GetTable("test1").ReturnResult as DataTable)
+                        .Query(id).ReturnResult as List<object>;
+                    foreach (var item in result)
+                    {
+                        Console.Write($"{item}\t");
+                    }
+                    Console.WriteLine();
+                    Assert.AreEqual(id, result[0]);
                 }
-                Console.WriteLine();
-                Assert.AreEqual(id, result[0]);
             }
         }
     }
