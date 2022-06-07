dad39e6025b1204540cba7a17736c4efd436cfa5
Dynesshely <catrol@qq.com>
2022-06-08 05:20:18 +0800

    💾 Feat: Platform

---
+++ BasicHelper/Util/Platform.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicHelper.Util
+{
+    public struct Platform
+    {
+
+        /// <summary>
+        /// 平台名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 平台版本
+        /// </summary>
+        public string Version { get; set; }
+
+        /// <summary>
+        /// 最小支持版本
+        /// </summary>
+        public Version MinistVersion { get; set; }
+    }
+}
