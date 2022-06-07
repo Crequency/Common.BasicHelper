1dfd4bde5520fb61e60bdacdfe130b9f05ed0609
Dynesshely <catrol@qq.com>
2022-06-08 05:30:43 +0800

    💾 Feat: Allow to hide the Build number

--- BasicHelper/Util/RegexStrings.cs
+++ BasicHelper/Util/RegexStrings.cs
@@ -6,6 +6,6 @@
 
         public const string LiteDB_Name_Limit = @"([A-Z]|[a-z]|[0-9])";
 
-        public const string Version_Parse_STR = @"[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*";
+        public const string Version_Parse_STR = @"[0-9]*\.[0-9]*\.[0-9]*(\.[0-9]*)?";
     }
 }
