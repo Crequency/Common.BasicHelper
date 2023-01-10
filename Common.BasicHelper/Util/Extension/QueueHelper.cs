8bf931805b7f41b6219d775383c0a7738cefb5e5
Dynesshely <catrol@qq.com>
2023-01-10 20:31:58 +0800

    💾 Feat: 为 Queue 添加两个扩展方法 1. Push 入队一个元素并返回队列本身, 适用于队列初始化 2. Pop 出队一个元素并返回队列本身, 适用于队列初始化

---
+++ Common.BasicHelper/Util/Extension/QueueHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Common.BasicHelper.Util.Extension
+{
+    public static class QueueHelper
+    {
+        /// <summary>
+        /// 入队一个元素并返回队列本身
+        /// </summary>
+        /// <typeparam name="T">队列类型</typeparam>
+        /// <param name="queue">队列</param>
+        /// <param name="item">元素</param>
+        /// <returns>队列</returns>
+        public static Queue<T> Push<T>(this Queue<T> queue, T item)
+        {
+            queue.Enqueue(item);
+            return queue;
+        }
+
+        /// <summary>
+        /// 出队一个元素并返回队列本身
+        /// </summary>
+        /// <typeparam name="T">队列类型</typeparam>
+        /// <param name="queue">队列</param>
+        /// <returns>队列</returns>
+        public static Queue<T> Pop<T>(this Queue<T> queue)
+        {
+            _ = queue.Dequeue();
+            return queue;
+        }
+    }
+}
