3c055bec5c5435981acb558195925786e0d818fe
Dynesshely <catrol@qq.com>
2023-01-25 04:37:46 +0800

    📝 📦 Chore, Struct: 项目架构调整

---
+++ Common.BasicHelper.Test/Util.cs
@@ -0,0 +1,14 @@
+﻿using Common.BasicHelper.Util;
+
+namespace Common.BasicHelper.Test;
+
+[TestClass]
+public class Util_Test
+{
+    [TestMethod]
+    public void GUID_Part_Random_Generate_Test()
+    {
+        for (int i = 0; i < 10; i++)
+            Console.WriteLine(GUID_Helper.Random_GUID_Generate().GetString());
+    }
+}
