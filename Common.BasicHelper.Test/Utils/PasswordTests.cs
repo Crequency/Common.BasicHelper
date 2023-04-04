9461fd8c912b189080c6f55369c2fb2862aebd07
Dynesshely <catrol@qq.com>
2023-04-04 10:35:39 +0800

    💾 ✔️ Feat, Test: Password class with generator.

---
+++ Common.BasicHelper.Test/Utils/PasswordTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Common.BasicHelper.Utils;
+
+[TestClass]
+public class PasswordTests
+{
+    [TestMethod]
+    public void Test_GeneratePassword()
+    {
+        foreach (var item in Enumerable.Range(0, 10))
+            Console.WriteLine(Password.GeneratePassword(length: 12));
+    }
+}
