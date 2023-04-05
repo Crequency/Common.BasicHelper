56754277347ce204e79477208d93f2e1c319ca07
Dynesshely <catrol@qq.com>
2023-04-05 19:00:50 +0800

    💾 ✔️ Feat, Test: More types supported to be `Close()`.
    Added types:
    
    - PipeStream
    - Process
    - Socket

---
+++ Common.BasicHelper.Test/Utils/Extensions/CleanHelperTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.IO.Pipes;
+using System.Net.Sockets;
+
+namespace Common.BasicHelper.Utils.Extensions;
+
+[TestClass]
+public class CleanHelperTests
+{
+    [TestMethod]
+    public void Test_CloseAndDispose()
+    {
+        // Arrange
+        var memoryStream = new MemoryStream();
+        var namedPipeServerStream = new NamedPipeServerStream("testpipe",
+            PipeDirection.InOut);
+        var binaryReader = new BinaryReader(memoryStream);
+        var binaryWriter = new BinaryWriter(memoryStream);
+        var streamReader = new StreamReader(memoryStream);
+        var streamWriter = new StreamWriter(memoryStream);
+        var process = new Process();
+        var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+        // Act
+        namedPipeServerStream.CloseAndDispose();
+        binaryReader.CloseAndDispose();
+        binaryWriter.CloseAndDispose();
+        streamReader.CloseAndDispose();
+        streamWriter.CloseAndDispose();
+        process.CloseAndDispose();
+        socket.CloseAndDispose();
+
+        memoryStream.CloseAndDispose();
+
+        // Assert
+        Assert.IsTrue(namedPipeServerStream.IsConnected == false);
+        Assert.IsTrue(binaryReader.BaseStream.CanRead == false);
+        Assert.IsTrue(binaryWriter.BaseStream.CanWrite == false);
+        Assert.IsTrue(streamReader.BaseStream.CanRead == false);
+        Assert.IsTrue(streamWriter.BaseStream.CanWrite == false);
+        Assert.IsTrue(socket.Connected == false);
+    }
+}
--- Common.BasicHelper/Utils/Extensions/CleanHelper.cs
+++ Common.BasicHelper/Utils/Extensions/CleanHelper.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.IO.Pipes;
 using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 
 namespace Common.BasicHelper.Utils.Extensions;
 
@@ -11,11 +15,29 @@ public static class CleanHelper
     /// </summary>
     /// <typeparam name="T">对象类型</typeparam>
     /// <param name="obj">对象</param>
-    public static void CloseAndDispose<T>(this T obj) where T : IDisposable
+    /// <param name="skipClose">是否跳过关闭</param>
+    public static void CloseAndDispose<T>(this T obj, bool skipClose = false) where T : IDisposable
     {
-        if (false) { }
+
+        if (skipClose) { }
 
         else if (obj is Stream stream) stream?.Close();
+
+        // Below types based on `Stream`
+        // 
+        // - FileStream
+        // - MemoryStream
+        // - CryptoStream
+        // - NetworkStream
+        // - DeflateStream
+        // - GZipStream
+
+        else if (obj is PipeStream pipeStream) pipeStream?.Close();
+
+        // Below types based on `PipeStream`:
+        // 
+        // - NamedPipeClientStream
+        // - NamedPipeServerStream
 
         else if (obj is BinaryReader binaryReader) binaryReader?.Close();
 
@@ -25,7 +47,16 @@ public static class CleanHelper
 
         else if (obj is TextWriter textWriter) textWriter?.Close();
 
+        // Below types based on `TextReader` or `TextWriter`
+        // 
+        // - StreamReader
+        // - StreamWriter
+
         else if (obj is WebResponse webResponse) webResponse?.Close();
+
+        else if (obj is Process process) process?.Close();
+
+        else if (obj is Socket socket) socket?.Close();
 
         obj.Dispose();
     }
