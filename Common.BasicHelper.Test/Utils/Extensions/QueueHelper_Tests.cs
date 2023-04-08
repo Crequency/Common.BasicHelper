517a9cefc45e9731a7e50e5ca0cac3a501e95755
Dynesshely <catrol@qq.com>
2023-04-09 00:48:21 +0800

    ✔️ Test: Update unit tests for `QueueHelper`

--- Common.BasicHelper.Test/Utils/Extensions/QueueHelper_Tests.cs
+++ Common.BasicHelper.Test/Utils/Extensions/QueueHelper_Tests.cs
@@ -1,4 +1,6 @@
-﻿namespace Common.BasicHelper.Utils.Extensions;
+﻿using System.Text;
+
+namespace Common.BasicHelper.Utils.Extensions;
 
 [TestClass]
 public class QueueHelper_Tests
@@ -36,5 +38,70 @@ public class QueueHelper_Tests
             .Push(5)
             ;
         Assert.AreEqual("3 4 5 ", queue.Dump());
+    }
+
+    [TestMethod()]
+    public void Test_ForEach()
+    {
+        var sb = new StringBuilder();
+
+        var queue = new Queue<int>()
+            .Push(1)
+            .Push(3)
+            .Push(5)
+            .ForEach(x => sb.AppendLine(x.ToString()), reappend: true)
+            ;
+
+        Assert.AreEqual
+        (
+            """
+            1
+            3
+            5
+
+            """,
+            sb.ToString().Print()
+        );
+    }
+
+    [TestMethod()]
+    public async Task Test_ForEachAsync()
+    {
+        var sb = new StringBuilder();
+
+        var queue = await new Queue<int>()
+            .Push(1)
+            .Push(3)
+            .Push(5)
+            .ForEachAsync(x => sb.AppendLine(x.ToString()), reappend: true)
+            ;
+
+        Assert.AreEqual
+        (
+            """
+            1
+            3
+            5
+
+            """,
+            sb.ToString().Print()
+        );
+    }
+
+    [TestMethod()]
+    public void Test_IsEmpty()
+    {
+        Assert.AreEqual(true, new Queue<int>().IsEmpty());
+
+        Assert.AreEqual
+        (
+            true,
+            new Queue<int>()
+                .Push(3)
+                .Push(5)
+                .Pop()
+                .Pop()
+                .IsEmpty()
+        );
     }
 }
