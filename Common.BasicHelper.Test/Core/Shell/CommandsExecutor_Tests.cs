e9d08de29c078b9296efb069e022d3f21e59978c
Dynesshely <catrol@qq.com>
2023-05-09 18:11:11 +0800

    🔧 Fix(Test): `help` command does not exist on other platforms, use other commands to replace.

--- Common.BasicHelper.Test/Core/Shell/CommandsExecutor_Tests.cs
+++ Common.BasicHelper.Test/Core/Shell/CommandsExecutor_Tests.cs
@@ -6,12 +6,26 @@ public class CommandsExecutor_Tests
     [TestMethod]
     public void Test_ExecuteAsCommand()
     {
-        Console.WriteLine("help".ExecuteAsCommand());
+        if (OperatingSystem.IsWindows())
+            Console.WriteLine("help".ExecuteAsCommand());
+
+        if (OperatingSystem.IsLinux())
+            Console.WriteLine("apt".ExecuteAsCommand("-v"));
+
+        if (OperatingSystem.IsMacOS())
+            Console.WriteLine("sw_vers".ExecuteAsCommand());
     }
 
     [TestMethod]
     public async Task Test_ExecuteAsCommandAsync()
     {
-        Console.WriteLine(await "help".ExecuteAsCommandAsync());
+        if (OperatingSystem.IsWindows())
+            Console.WriteLine(await "help".ExecuteAsCommandAsync());
+
+        if (OperatingSystem.IsLinux())
+            Console.WriteLine(await "apt".ExecuteAsCommandAsync("-v"));
+
+        if (OperatingSystem.IsMacOS())
+            Console.WriteLine(await "sw_vers".ExecuteAsCommandAsync());
     }
 }
