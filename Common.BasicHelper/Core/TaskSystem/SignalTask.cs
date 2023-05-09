714f91904810293e10aed0b1b96a58bb2953553f
Dynesshely <catrol@qq.com>
2023-05-09 17:17:55 +0800

    💾 Feat(TaskSystem): Import `SignalTask` system from KitX.

---
+++ Common.BasicHelper/Core/TaskSystem/SignalTask.cs
@@ -0,0 +1,63 @@
+﻿using Common.BasicHelper.Utils.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Common.BasicHelper.Core.TaskSystem;
+
+public class SignalTask
+{
+    public Queue<Action> Actions { get; } = new();
+
+    public bool SignalReusable { get; set; } = false;
+
+    public bool ActionsReusable { get; set; } = false;
+
+    public SignalTask AddAction(Action action)
+    {
+        Actions.Enqueue(action);
+
+        return this;
+    }
+
+    public SignalTask ReuseSignal(bool reusable = true)
+    {
+        SignalReusable = reusable;
+
+        return this;
+    }
+
+    public SignalTask ReuseActions(bool reusable = true)
+    {
+        ActionsReusable = reusable;
+
+        return this;
+    }
+
+    public SignalTask ExecuteAll(bool? reuseall = null)
+    {
+        Actions.ForEach(action => action(), reuseall ?? ActionsReusable);
+
+        return this;
+    }
+
+    public async Task<SignalTask> ExecuteAllAsync(bool? reuseall = null, CancellationToken? token = null)
+    {
+        await Actions.ForEachAsync(
+            action => action(),
+            reuseall ?? ActionsReusable,
+            token ?? default
+        );
+
+        return this;
+    }
+}
+
+public static class SignalTaskExtensions
+{
+    public static SignalTask AddTo(this Action action, SignalTask task) => task.AddAction(action);
+
+    public static SignalTask ExecuteAll(this SignalTask task, bool reuseAll = false)
+        => task.ExecuteAll(reuseAll);
+}
---
+++ Common.BasicHelper/Core/TaskSystem/SignalTasksManager.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Common.BasicHelper.Core.TaskSystem;
+
+public class SignalTasksManager
+{
+    private readonly Dictionary<string, SignalTask> SignalTasks = new();
+
+    /// <summary>
+    /// 触发信号
+    /// </summary>
+    /// <param name="signal">信号名称</param>
+    /// <returns>任务管理器本身</returns>
+    internal SignalTasksManager RaiseSignal(string signal)
+    {
+        if (SignalTasks.TryGetValue(signal, out var signalTask))
+        {
+            signalTask.ExecuteAll();
+
+            if (!signalTask.SignalReusable)
+                SignalTasks.Remove(signal);
+        }
+
+        return this;
+    }
+
+    /// <summary>
+    /// 信号发生时运行任务
+    /// </summary>
+    /// <param name="signal">信号</param>
+    /// <param name="action">任务</param>
+    /// <returns>任务管理器本身</returns>
+    internal SignalTasksManager SignalRun(string signal, Action action, bool reusable = false)
+    {
+        if (SignalTasks.TryGetValue(signal, out var signalTask))
+            signalTask.AddAction(action);
+        else
+            SignalTasks.Add(
+                signal,
+                new SignalTask()
+                    .ReuseSignal(reusable)
+                    .AddAction(action)
+            );
+
+        return this;
+    }
+
+    /// <summary>
+    /// 清除所有信号响应任务
+    /// </summary>
+    /// <returns>任务管理器本身</returns>
+    internal SignalTasksManager Clear()
+    {
+        SignalTasks.Clear();
+
+        return this;
+    }
+
+    /// <summary>
+    /// 执行任务, 并带有更好的日志显示
+    /// </summary>
+    /// <param name="action">要执行的动作</param>
+    /// <param name="name">日志显示名称</param>
+    /// <param name="prompt">日志提示</param>
+    public static void RunTask(
+        Action action,
+        string name = nameof(Action),
+        string prompt = ">>> ",
+        bool catchException = false,
+        Action<string>? infoLogger = null,
+        Action<Exception, string>? errLogger = null)
+    {
+        infoLogger?.Invoke($"{prompt}Task `{name}` began.");
+
+        if (catchException)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                errLogger?.Invoke(e, $"{prompt}Task `{name}` failed: {e.Message}");
+            }
+        }
+        else action();
+
+        infoLogger?.Invoke($"{prompt}Task `{name}` done.");
+    }
+
+    /// <summary>
+    /// 异步执行任务, 并带有更好的日志显示
+    /// </summary>
+    /// <param name="action">要执行的动作</param>
+    /// <param name="name">任务名称</param>
+    /// <param name="prompt">日志提示</param>
+    public static async Task RunTaskAsync(
+        Action action,
+        string name = nameof(Action),
+        string prompt = ">>> ",
+        bool catchException = false,
+        Action<string>? infoLogger = null,
+        Action<Exception, string>? errLogger = null)
+    {
+        infoLogger?.Invoke($"{prompt}Task `{name}` began.");
+
+        if (catchException)
+        {
+            try
+            {
+                await Task.Run(action);
+            }
+            catch (Exception e)
+            {
+                errLogger?.Invoke(e, $"{prompt}Task `{name}` failed: {e.Message}");
+            }
+        }
+        else await Task.Run(action);
+
+        infoLogger?.Invoke($"{prompt}Task `{name}` done.");
+    }
+}
