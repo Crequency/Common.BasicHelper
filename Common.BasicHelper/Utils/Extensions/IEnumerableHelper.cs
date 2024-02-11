390383ab50ac83488101ab9ed03cbed3b879e3dc
Dynesshely <catrol@qq.com>
2024-02-12 05:05:41 +0800

    💾 Feat(Extensions): An extension for `IEnumerable` that checks its count.

---
+++ Common.BasicHelper/Utils/Extensions/IEnumerableHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Common.BasicHelper.Utils.Extensions;
+
+public static class IEnumerableHelper
+{
+    public static IEnumerable<T> AssertCount<T>(this IEnumerable<T> items, int count, Exception? exception = null)
+    {
+        var current = items.Count();
+
+        if (current != count)
+            throw exception ?? new ArgumentException(
+                $"Count ({current}) not match {count}",
+                nameof(items)
+            );
+
+        return items;
+    }
+}
