5353ae2d63b3d1629c38c3ae580f56e2c3a8b3e7
Dynesshely <catrol@qq.com>
2024-04-06 10:41:34 +0800

    💾 Feat(Extensions): Added a extension method for `Match` in regex

---
+++ Common.BasicHelper/Utils/Extensions/RegexHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Common.BasicHelper.Utils.Extensions;
+
+public static class RegexHelper
+{
+    public static Match? WhenSuccess(this Match? match, Action<Match?> action)
+    {
+        if (match?.Success ?? false) action?.Invoke(match);
+
+        return match;
+    }
+}
--- Common.BasicHelper/Utils/Password.cs
+++ Common.BasicHelper/Utils/Password.cs
@@ -17,21 +17,6 @@ public class Password
 
     public const string AllSymbols = @"!@#$%^&*()_+-=[]{};':,./<>?";
 
-    /// <summary>
-    /// 生成密码
-    /// </summary>
-    /// <param name="length">确切长度</param>
-    /// <param name="lengthRangeStart">长度范围始</param>
-    /// <param name="lengthRangeEnd">长度范围终</param>
-    /// <param name="includeUppercase">是否包含大写字母</param>
-    /// <param name="includeLowercase">是否包含小写字母</param>
-    /// <param name="includeNumbers">是否包含数字</param>
-    /// <param name="includeSymbols">是否包含符号</param>
-    /// <param name="supportedUppercases">支持的大写字母集</param>
-    /// <param name="supportedLowercases">支持的小写字母集</param>
-    /// <param name="supportedNumbers">支持的数字集</param>
-    /// <param name="supportedSymbols">支持的符号集</param>
-    /// <returns>生成的密码</returns>
     public static string GeneratePassword
     (
         int? length = null,
