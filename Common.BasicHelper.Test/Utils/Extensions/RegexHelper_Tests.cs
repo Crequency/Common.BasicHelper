7b194286d46bdf99883337bac5b74d4779ff0b32
Dynesshely <catrol@qq.com>
2024-04-06 10:49:59 +0800

    ✔️ Test(Extensions): Succeed int `WhenSuccess` method for `Match`

---
+++ Common.BasicHelper.Test/Utils/Extensions/RegexHelper_Tests.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Common.BasicHelper.Utils.Extensions;
+
+[TestClass]
+public class RegexHelper_Tests
+{
+    [TestMethod]
+    public void Test_WhenSuccess()
+    {
+#pragma warning disable SYSLIB1045 // 转换为“GeneratedRegexAttribute”。
+        _ = Regex.Match("Hello World", "^(Hello).*$").WhenSuccess(
+            x => Assert.AreEqual("Hello", x?.Groups[1].Value)
+        );
+#pragma warning restore SYSLIB1045 // 转换为“GeneratedRegexAttribute”。
+    }
+}
