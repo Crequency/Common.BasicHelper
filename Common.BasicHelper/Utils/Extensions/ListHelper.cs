8e6051ad015afcc4f3062e5a27329a3ffa6eddee
Dynesshely <catrol@qq.com>
2024-06-17 14:16:31 +0800

    🎇 Style: Clean and fix typo

--- Common.BasicHelper/Utils/Extensions/ListHelper.cs
+++ Common.BasicHelper/Utils/Extensions/ListHelper.cs
@@ -5,26 +5,19 @@ namespace Common.BasicHelper.Utils.Extensions;
 
 public static class ListHelper
 {
-    /// <summary>
-    /// 转换到自定义字符串
-    /// </summary>
-    /// <typeparam name="T">列表类型</typeparam>
-    /// <param name="list">列表</param>
-    /// <param name="separater">分隔符</param>
-    /// <returns>自定义字符串</returns>
-    public static string ToCustomString<T>(this List<T> list, string separater = ",", bool cutEnding = true)
+    public static string ToCustomString<T>(this List<T> list, string separator = ",", bool cutEnding = true)
     {
         var sb = new StringBuilder();
 
         foreach (var item in list)
         {
-            sb.Append(item?.ToString());
-            sb.Append(separater);
+            sb.Append(item);
+            sb.Append(separator);
         }
 
         var result = sb.ToString();
 
-        return cutEnding ? result[0..(result.Length - separater.Length)] : result;
+        return cutEnding ? result[..^separator.Length] : result;
     }
 
 }
